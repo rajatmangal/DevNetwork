8ae3b6a4e87e07d3b319c5831cc0b65fa0e3a835
Rajat Mangal <rmangal@sfu.ca>
2020-06-06 00:23:01 -0700

    Mongo Setup

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+//Connect To DB
+connectDB();
 
 app.get("/", (req, res) => {
-  res.send("API Running");
+    res.send("API Running");
 });
 
+
+
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server started on PORT ${PORT}`);
+    console.log(`Server started on PORT ${PORT}`);
 });
