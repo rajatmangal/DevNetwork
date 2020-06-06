eef7c35a36b95514c7cfee33ffa669d0633cd03f
Rajat Mangal <rmangal@sfu.ca>
2020-06-06 12:43:36 -0700

    Protected Routes added

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const auth = require("../../middlewares/auth");
+const User = require("../../models/Users");
 const router = express.Router();
 
 //@route GET api/auth
 //@desc Test Route
-//@access Public
-router.get('/', (req, res) => {
-    res.send("Auth route");
+//@access Protected
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Server Error");
+    }
 })
 
 module.exports = router;
