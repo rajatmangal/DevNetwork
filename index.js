91acbff9a9f0299c3a7300bac3a4d7fde85adeb7
Rajat Mangal <rmangal@sfu.ca>
2020-06-07 22:18:35 -0700

    gitignore file fixed

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ const app = express();
 //Connect To DB
 connectDB();
 
-
 //Init Middleware
-app.use(express.json({
-    extended: false
-}));
+app.use(
+  express.json({
+    extended: false,
+  })
+);
 
 app.get("/", (req, res) => {
-    res.send("API Running");
+  res.send("API Running");
 });
 
-
 //Define Routes
-app.use('/api/users', require("./routes/api/users"));
-app.use('/api/auth', require("./routes/api/auth"));
-app.use('/api/profile', require("./routes/api/profile"));
-app.use('/api/posts', require("./routes/api/posts"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server started on PORT ${PORT}`);
+  console.log(`Server started on PORT ${PORT}`);
 });
